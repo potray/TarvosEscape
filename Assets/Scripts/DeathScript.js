2de1ffd3bb7dec266136da3db5a8a20f826cdd21
Daniel <potrayarrick@gmail.com>
2014-03-23 20:18:47 +0100

    Añadidas algunas plataformas, creado plano de la muerte, si te caes de una plataforma vuelves a donde te caíste.

---
+++ Assets/Scripts/DeathScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+static var returnPosition : Vector3;
+var platformScript : PlatformScript;
+
+function Start () {
+
+}
+
+function Update () {
+}
+
+function OnTriggerEnter (){
+	print ("Trigger de prueba: se reinicia el nivel");	
+	var playerObject = GameObject.Find("Player");
+	print ("Moviendo jugador a: " + returnPosition);
+	playerObject.transform.position = returnPosition;
+}
+
+function setReturnPosition(newReturnPosition : Vector3){
+	returnPosition = newReturnPosition;
+	print ("Nueva posicion de retorno: " + returnPosition);
+}
---
+++ Assets/Scripts/PlatformScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var deathScript : DeathScript;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerExit(){
+	print("Salido de plataforma");
+	var playerObject = GameObject.Find("Player");
+	var playerPosition : Vector3 = playerObject.transform.position;
+	deathScript.setReturnPosition(playerPosition);	
+}
