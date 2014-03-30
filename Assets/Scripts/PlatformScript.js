2ed433530a750df700a474a6a3cf4f792652ab87
Daniel <potrayarrick@gmail.com>
2014-03-31 01:14:32 +0200

    Creada una plataforma sube-baja, agregada posibilidad de otras plataformas movibles al script de plataformas.

--- Assets/Scripts/PlatformScript.js
+++ Assets/Scripts/PlatformScript.js
@@ -1,13 +1,35 @@
 ﻿#pragma strict
 
 var deathScript : DeathScript;
+enum PlatformType{normal, upDown, leftRight, rotating};
+var platformType : PlatformType;
+var initialPosition : Vector3;
+var mode : boolean = true;
+
 
 function Start () {
-
+	initialPosition = this.gameObject.transform.position;
 }
 
 function Update () {
-
+	switch (platformType){
+		case PlatformType.upDown:
+			//Mode: true -> arriba, false -> abajo.
+			var directionVector : Vector3;
+			if (this.gameObject.transform.position.y > initialPosition.y + 10)		
+				mode = false;	
+			else if (this.gameObject.transform.position.y < initialPosition.y)	
+				mode = true;
+				
+			if (mode)
+				directionVector = Vector3.up;
+			else
+				directionVector = Vector3.down;
+				
+			this.gameObject.transform.Translate (directionVector * Time.deltaTime * 5);
+		break;
+		case PlatformType.normal: break;
+	}
 }
 
 function OnTriggerExit(){
