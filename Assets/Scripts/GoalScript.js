d594b204f8744d54d0a25156adeca1d09601e5dc
Daniel <potrayarrick@gmail.com>
2014-04-02 15:38:39 +0200

    Meta chustera

---
+++ Assets/Scripts/GoalScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (coll : Collider){
+	print("El jugador " + coll.name + " ha ganado");
+	//Mostrar pantalla de fin de juego
+	
+	//TEMPORAL
+	Application.LoadLevel("MainMenu");
+}
