639b6ed9095eb746b1e3b691a72eb36c3fc74891
David Téllez Rodríguez <davtelrod@gmail.com>
2014-04-22 21:15:31 +0200

    Menú de pausa versión 0.1

--- Assets/Scripts/AsteroidsMovementScript.js
+++ Assets/Scripts/AsteroidsMovementScript.js
@@ -7,28 +7,7 @@ function Start () {
 }
 
 function Update () {
-/*
-	switch (name){
-		case "Big Asteroids":
-			speed = 0.15;
-			break;
-		case "Big Asteroids 2":
-			speed = 0.2;
-			break;
-		case "Medium Asteroids":
-			speed = 0.3;
-			break;
-		case "Medium Asteroids 2":
-			speed = 0.35;
-			break;
-		case "Small Asteroids":
-			speed = 0.6;
-			break;
-		case "Small Asteroids 2":
-			speed = 0.65;
-			break;	
-	}
-*/	
+
 	if(transform.position.x > 12) {
 		transform.position = Vector3(-18,transform.position.y,transform.position.z);
 	}
---
+++ Assets/Scripts/PauseMenu.js
@@ -0,0 +1,64 @@
+﻿#pragma strict
+
+var isPause = false;
+var font : Font;
+var inputController : FPSInputController;
+var camX : MouseLook;
+var camY : MouseLook;
+
+function quitMenu () {
+	Time.timeScale = 1;
+	(inputController as MonoBehaviour).enabled = true;
+	camX.enabled = true;
+	camY.enabled = true;
+	isPause = false;
+}
+
+ 
+function Update () {
+
+	if(Input.GetKeyDown(KeyCode.Escape))
+	{
+		isPause = !isPause;
+		if(isPause) {
+			Time.timeScale = 0;
+			(inputController as MonoBehaviour).enabled = false;
+			camX.enabled = false;
+			camY.enabled = false;
+
+		}
+		else {
+			quitMenu();
+		}
+	}
+	
+}
+
+function OnGUI() {
+
+	if (isPause) {
+	 	GUI.skin.label.font = GUI.skin.button.font = GUI.skin.box.font = font;
+		GUI.skin.label.fontSize = GUI.skin.box.fontSize = GUI.skin.button.fontSize = 16;
+		
+		if (GUI.Button(Rect(Screen.width/2-30,Screen.height/2-70,100,30),"Resume")) {
+			// Boton de vuelta a la partida.
+			print("Resume.");
+			quitMenu();
+		}
+		if (GUI.Button(Rect(Screen.width/2-30,Screen.height/2-20,100,30),"Options")) {
+			// Boton de opciones.
+			print("Options.");
+		}
+		if (GUI.Button(Rect(Screen.width/2-30,Screen.height/2+30,100,30),"Main Menu")) {
+			// Boton de salir al menu principal.
+			print("Main Menu.");
+			Application.LoadLevel(0);
+			quitMenu();
+		}
+	}
+}
+
+ 
+function Start () {
+	
+}
