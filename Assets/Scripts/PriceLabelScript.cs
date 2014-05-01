e224e94282a1704078997e04219b80e94483f681
Daniel <potrayarrick@gmail.com>
2014-05-01 14:06:55 +0200

    Sistema de mejoras 100% operativo

---
+++ Assets/Scripts/PriceLabelScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PriceLabelScript : MonoBehaviour {
+
+	UpgradeButtonScript upgradeScript;
+	UILabel label;
+	int currentUpgrade, lastUpgrade = -1;
+
+	// Use this for initialization
+	void Start () {
+		upgradeScript = (UpgradeButtonScript) transform.parent.gameObject.GetComponent("UpgradeButtonScript");
+		label = GetComponent <UILabel> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentUpgrade = upgradeScript.upgrade;
+		if (lastUpgrade != currentUpgrade)
+			updateText ();
+	}
+
+	void updateText(){
+		lastUpgrade = currentUpgrade;
+		if (currentUpgrade != upgradeScript.maxUpgrades)
+			label.text = upgradeScript.getPrices () [currentUpgrade].ToString ();
+		else
+			label.text ="Max";
+	}
+}
