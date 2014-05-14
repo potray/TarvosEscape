951f5a437a3d703684b9b9017bb1ff12de77792f
Daniel <potrayarrick@gmail.com>
2014-05-14 15:25:50 +0200

    Arreglado el problema de la GUI en el juego y las rotaciones raras, añadidos pickables de alas (jetpack) y escudo, medio hecho el pasillo campo de batalla.

---
+++ Assets/Standard Assets/Scripts/EnemyInventoryScript.js
@@ -0,0 +1,76 @@
+﻿#pragma strict
+
+static var enemyCoins : int = 0;
+static var enemyItem : ItemType;
+var animPlayer : Animator;
+
+// the tag to search for (set this value in the inspector)
+var searchTag = "PCoin";
+
+// the frequency with which to re-scan for new nearest target in seconds
+// (set in inspector)
+var scanFrequency = 1.0;
+
+// the current target
+private var target : Transform;
+
+
+function Start() {
+	// set up repeating scan for new targets:
+	InvokeRepeating("ScanForTarget", 0, scanFrequency );
+	enemyCoins = 0;
+}
+
+function Update() {
+	// we rotate to look at the target every frame (if there is one)
+	if (target != null) {
+		transform.LookAt(target);
+		
+		//  Hacemos que ande hacia el objeto mas cercano.
+		var dist = (target.transform.position - transform.position).sqrMagnitude;
+		
+	if (dist  > 0.2) {
+			animPlayer.SetFloat("Walk", 0.2);
+		}
+		else
+			animPlayer.SetFloat("Walk", 0);
+
+	}
+}
+
+function ScanForTarget() {
+	// this should be called less often, because it could be an expensive
+	// process if there are lots of objects to check against
+	target = GetNearestTaggedObject();
+}
+
+function GetNearestTaggedObject() : Transform {
+	// and finally the actual process for finding the nearest object:
+
+	var nearestDistanceSqr = Mathf.Infinity;
+	var taggedGameObjects = GameObject.FindGameObjectsWithTag(searchTag);
+	var nearestObj : Transform = null;
+
+	// loop through each tagged object, remembering nearest one found
+	for (var obj : GameObject in taggedGameObjects) {
+		var objectPos = obj.transform.position;
+		var distanceSqr = (objectPos - transform.position).sqrMagnitude;
+
+		if (distanceSqr < nearestDistanceSqr) {
+			nearestObj = obj.transform;
+			nearestDistanceSqr = distanceSqr;
+		}
+	}
+
+	return nearestObj;
+}
+
+function setItem (newItem : ItemType){
+	if (newItem == ItemType.Coin) {
+			enemyCoins++;
+			print("Monedas (enemigo): " + enemyCoins);
+		}
+	else
+		enemyItem = newItem;
+}
+
---
+++ Assets/Standard Assets/Scripts/PlayerInventoryScript.js
@@ -0,0 +1,79 @@
+﻿#pragma strict
+
+//He descubirto que tiene que ser estatica si o si, si no no funciona.
+static var Item : ItemType;
+static var playerCoins : int = 0;
+var bulletPrefab : GameObject;
+var rocketPrefab : GameObject;
+var bulletInitialSpeed : float = 20f;
+var knifeClip : AudioClip;
+var rocketLauncherClip : AudioClip;
+var gunClip : AudioClip;
+
+// Para que las monedas se guarden entre niveles:
+// var playerInventoryScript : PlayerInventoryScript;
+// Y referenciar usando playerInventoryScript.playerCoins
+
+
+function Start () {
+	Item = ItemType.Empty;
+	playerCoins = 0;
+}
+
+function getPlayerCoins(){
+	return playerCoins;
+}
+
+function Update () {
+
+	if (Input.GetButtonDown("Fire1")){
+		switch(Item){
+			case ItemType.Gun:			
+				print("Usado gun");
+				//Lo lanzo hacia donde apunte la camara
+				var cam : Camera = Camera.main;
+				var bullet : GameObject = Instantiate(bulletPrefab, cam.transform.position + cam.transform.forward, cam.transform.rotation);
+				bullet.rigidbody.AddForce(cam.transform.forward * bulletInitialSpeed, ForceMode.Impulse);
+				AudioSource.PlayClipAtPoint(gunClip, Camera.main.transform.position);					
+				break;
+			case ItemType.RocketLauncher:
+				print("Usado lanzacohetes");
+				var camRocketLauncher : Camera = Camera.main;
+				var rocket : GameObject = Instantiate(rocketPrefab, camRocketLauncher.transform.position + camRocketLauncher.transform.forward, camRocketLauncher.transform.rotation);
+				rocket.rigidbody.AddForce(camRocketLauncher.transform.forward * bulletInitialSpeed * 2, ForceMode.Impulse);
+				AudioSource.PlayClipAtPoint(rocketLauncherClip, Camera.main.transform.position);	
+				break;
+			case ItemType.Shield:
+				//Temporal
+				print("Usado escudo");
+				break;
+			case ItemType.Gravigun:
+				//Temporal
+				print("Usado gravigun");
+				break;
+			case ItemType.Knife:
+				//Temporal
+				print("Usado cuchillo");				
+				AudioSource.PlayClipAtPoint(knifeClip, Camera.main.transform.position);	
+				break;
+			case ItemType.Wings:
+				//Temporal
+				print("Usado alas");
+				break;
+		}
+		emptyItem();
+	}
+}
+
+function setItem (newItem : ItemType){
+	if (newItem == ItemType.Coin) {
+			playerCoins++;
+			print("Monedas: " + playerCoins);
+		}
+	else 
+		Item = newItem;
+}
+
+function emptyItem (){
+	Item = ItemType.Empty;
+}
