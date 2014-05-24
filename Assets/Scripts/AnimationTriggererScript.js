d338e2062cabcf95fadf2c0f736ba83cd233de3e
Daniel <potrayarrick@gmail.com>
2014-05-24 21:52:39 +0200

    Segundo nivel terminado.

--- Assets/Scripts/AnimationTriggererScript.js
+++ Assets/Scripts/AnimationTriggererScript.js
@@ -3,6 +3,12 @@
 var anim : Animation;
 var rewindTime : float;
 
+enum TriggerType {Normal, Activated};
+
+var type : TriggerType = TriggerType.Normal;
+var animated = false;
+var activationAudio : AudioClip;
+
 function Start () {
 }
 
@@ -10,12 +16,28 @@ function Update () {
 }
 
 function OnTriggerEnter (){
-	print("animando");
-	anim.Play();
-	yield WaitForSeconds(rewindTime);
-	//Le cambio la velocidad y el tiempo del clip de la animacion y luego la animo.
-	anim.animation[anim.animation.clip.name].speed = -0.1;
-	anim.animation[anim.animation.clip.name].time = anim.animation[anim.animation.clip.name].length;
-	anim.Play();
-	
+	switch (type){
+		case TriggerType.Normal :
+			anim.Play();
+			yield WaitForSeconds(rewindTime);
+			//Le cambio la velocidad y el tiempo del clip de la animacion y luego la animo.
+			anim.animation[anim.animation.clip.name].speed = -0.1;
+			anim.animation[anim.animation.clip.name].time = anim.animation[anim.animation.clip.name].length;
+			anim.Play();		
+		break;			
+	}	
+}
+
+function OnTriggerStay (){	
+	switch (type){
+		case TriggerType.Activated:
+			var actionPressed = Input.GetKeyDown(KeyCode.E);			
+			if (actionPressed && !animated){
+				anim.Play();
+				if (activationAudio != null)
+					AudioSource.PlayClipAtPoint(activationAudio, transform.position);
+				animated = true;
+			}				
+		break;
+	}
 }
