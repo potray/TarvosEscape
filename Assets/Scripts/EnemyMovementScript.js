b2bc441e13f25bb14174a8dbf76bdbd2677ba9b2
David Téllez Rodríguez <davtelrod@gmail.com>
2014-05-25 23:15:28 +0200

    Prueba de movimiento del enemigo

---
+++ Assets/Scripts/EnemyMovementScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var agent : NavMeshAgent ;
+var targetPoint : Vector3;
+var baseDistance : float;
+
+function Start () {
+	baseDistance = 1;
+	agent = GetComponent.<NavMeshAgent>();
+	targetPoint = new Vector3 (-515, 500.5, -500);
+}
+
+function Update () {
+	agent.SetDestination(targetPoint);
+
+}
---
+++ Assets/Scripts/EnemyNavigationScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var agent : NavMeshAgent ;
+var targetPoint : Vector3;
+
+function Start () {
+	agent = GetComponent.<NavMeshAgent>();
+	targetPoint = new Vector3 (-10, 6, 110);
+}
+
+function Update () {
+	agent.SetDestination(targetPoint);
+
+}
