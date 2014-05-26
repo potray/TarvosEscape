fc0593569049b70ac165261231a3459826f60e72
Daniel <potrayarrick@gmail.com>
2014-05-26 18:45:56 +0200

    Añadidos detallitos de luces.

---
+++ Assets/Scripts/IntermitentLightScript.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var frequency : double;
+var myLight : Light;
+var shouldIFlick : boolean;
+
+function Start () {
+	myLight = GetComponent("Light");
+	shouldIFlick = true;
+}
+
+function Update () {	
+	flick();	
+}
+
+function flick (){
+	if (shouldIFlick){
+		myLight.enabled = !myLight.enabled;	
+		shouldIFlick = false;
+		yield WaitForSeconds(frequency);
+		shouldIFlick = true;
+	}
+}
