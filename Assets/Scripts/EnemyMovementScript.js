3130e247e0ff51f002defbce69636bab394eefe3
David Téllez Rodríguez <davtelrod@gmail.com>
2014-05-27 10:21:33 +0200

    Enemigo saltando

--- Assets/Scripts/EnemyMovementScript.js
+++ Assets/Scripts/EnemyMovementScript.js
@@ -4,12 +4,16 @@ var agent : NavMeshAgent ;
 var targetPoint : Vector3;
 var baseDistance : float;
 var rigid : Rigidbody;
+var jumpX : double;
+var jumpY : double;
+var jumpZ : double;
+var waitTime : double;
 
 function Start () {
 	baseDistance = 1;
 	agent = GetComponent.<NavMeshAgent>();
-	targetPoint = new Vector3 (-515, 500.5, -500);
-	rigid = GetComponent.<Rigidbody>();
+	//targetPoint = new Vector3 (-515, 500.5, -500);
+	rigid = GetComponent("Rigidbody");
 	agent.SetDestination(targetPoint);
 	//agent.Stop(true);
 
@@ -18,13 +22,22 @@ function Start () {
 function Update () {
 	if (Input.GetKeyDown(KeyCode.J))
 		jmp();
+	if (Input.GetKeyDown(KeyCode.K)){
+		//agent.ResetPath();
+		agent.enabled = true;
+		agent.SetDestination(targetPoint);
+		//agent.Resume();
+	}
+	
+	if (Input.GetKeyDown(KeyCode.H))
+		agent.CompleteOffMeshLink();
+		
 }
 
 function jmp () {
-	agent.Stop(true);
     rigid.isKinematic = false;
     rigid.useGravity = true;
-    rigid.AddForce(new Vector3(0,20,0),ForceMode.Impulse);
-    
-    print("H");
+    rigid.AddRelativeForce(new Vector3(0,3,3.5),ForceMode.Impulse);
+	//agent.Stop(true);
+	agent.enabled = false;
 }
